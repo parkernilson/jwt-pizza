d335d3659fa99d4aebce31a693acb7dc2d5590aa
Parker Nilson <parker.todd.nilson@gmail.com>
2024-10-10 17:17:55 -0600

    #d4: admin and franchisee tests

---
+++ tests/admin.spec.ts
@@ -0,0 +1,107 @@
+import { test, expect } from "playwright-test-coverage";
+
+const loginAdminReq = { email: "a@jwt.com", password: "admin" };
+
+const loginAdminRes = {
+  user: {
+    id: 1,
+    name: "常用名字",
+    email: "a@jwt.com",
+    roles: [
+      {
+        role: "admin",
+      },
+    ],
+  },
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IuW4uOeUqOWQjeWtlyIsImVtYWlsIjoiYUBqd3QuY29tIiwicm9sZXMiOlt7InJvbGUiOiJhZG1pbiJ9XSwiaWF0IjoxNzI4NTA2MDIwfQ.RyvKeThCxvkSZVxfDuEkYsSa41on0Ow1McfaIusoyb8",
+};
+
+const getAllFranchises_store1 = [
+  {
+    id: 2,
+    name: "Franchise 1",
+    admins: [
+      {
+        id: 2,
+        name: "Franchisee One",
+        email: "Franchisee1@gmail.com",
+      },
+    ],
+    stores: [
+      {
+        id: 4,
+        name: "Store 1",
+        totalRevenue: 0,
+      },
+    ],
+  },
+];
+
+const deleteFranchiseRes = { message: "franchise deleted" };
+
+test("Admin can view franchises", async ({ page }) => {
+  await page.route("**/*/api/auth", async (route) => {
+    expect(route.request().postDataJSON()).toStrictEqual(loginAdminReq);
+    expect(route.request().method()).toBe("PUT");
+    await route.fulfill({ json: loginAdminRes });
+  });
+
+  await page.route("**/*/api/franchise", async (route) => {
+    expect(route.request().method()).toBe("GET");
+    await route.fulfill({ json: getAllFranchises_store1 });
+  });
+
+  await page.goto("/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByPlaceholder("Email address").click();
+  await page.getByPlaceholder("Email address").fill("a@jwt.com");
+  await page.getByPlaceholder("Email address").press("Tab");
+  await page.getByPlaceholder("Password").fill("admin");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("link", { name: "Admin" }).click();
+  await expect(page.locator("tbody")).toContainText("Franchise 1");
+  await expect(page.locator("tbody")).toContainText("Franchisee One");
+  await expect(page.locator("tbody")).toContainText("Store 1");
+  await expect(page.locator("tbody")).toContainText("0 ₿");
+});
+
+test("Admin can delete a franchise", async ({ page }) => {
+  await page.route("**/*/api/auth/login", async (route) => {
+    expect(route.request().postDataJSON()).toStrictEqual(loginAdminReq);
+    expect(route.request().method()).toBe("PUT");
+    await route.fulfill({ json: loginAdminRes });
+  });
+
+  await page.route("**/*/api/franchise", async (route) => {
+    expect(route.request().method()).toBe("GET");
+    await route.fulfill({ json: [] });
+  }, { times: 1 });
+
+  await page.route("**/*/api/franchise", async (route) => {
+    expect(route.request().method()).toBe("GET");
+    await route.fulfill({ json: getAllFranchises_store1 });
+  }, { times: 1 });
+
+  await page.route("**/*/api/franchise/2", async (route) => {
+    expect(route.request().method()).toBe("DELETE");
+    await route.fulfill({ json: deleteFranchiseRes });
+  });
+
+  await page.goto("/");
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('a@jwt.com');
+  await page.getByPlaceholder('Email address').press('Tab');
+  await page.getByPlaceholder('Password').fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await expect(page.locator('tbody')).toContainText('Franchise 1');
+  await expect(page.locator('tbody')).toContainText('Franchisee One');
+  await expect(page.locator('tbody')).toContainText('Store 1');
+  await expect(page.locator('tbody')).toContainText('0 ₿');
+  await page.getByRole('row', { name: 'Franchise 1 Franchisee One' }).getByRole('button').click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+});
+
---
+++ tests/franchisee.spec.ts
@@ -0,0 +1,74 @@
+import { test, expect } from "@playwright/test";
+
+const loginFranchisee1Req = {
+  email: "franchisee1@gmail.com",
+  password: "password123",
+};
+
+const loginFranchisee1Res = {
+  user: {
+    id: 2,
+    name: "Franchisee One",
+    email: "Franchisee1@gmail.com",
+    roles: [
+      {
+        role: "diner",
+      },
+      {
+        objectId: 5,
+        role: "franchisee",
+      },
+    ],
+  },
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkZyYW5jaGlzZWUgT25lIiwiZW1haWwiOiJGcmFuY2hpc2VlMUBnbWFpbC5jb20iLCJyb2xlcyI6W3sicm9sZSI6ImRpbmVyIn0seyJvYmplY3RJZCI6NSwicm9sZSI6ImZyYW5jaGlzZWUifV0sImlhdCI6MTcyODYwMDg5MX0.MO3554iE2ViOZFD-i5qZ4Y6gD8iZz1hdBvUPh0u8qjs",
+};
+
+const getFranchises_franchisee1_store1 = [
+  {
+    id: 5,
+    name: "Franchise 1",
+    admins: [
+      {
+        id: 2,
+        name: "Franchisee One",
+        email: "Franchisee1@gmail.com",
+      },
+    ],
+    stores: [
+      {
+        id: 7,
+        name: "Store 1",
+        totalRevenue: 0,
+      },
+    ],
+  },
+];
+
+test("Franchisee can view their own franchise and stores", async ({ page }) => {
+  await page.route("**/*/api/auth", async (route) => {
+    expect(route.request().postDataJSON()).toStrictEqual(loginFranchisee1Req);
+    expect(route.request().method()).toBe("PUT");
+    await route.fulfill({ json: loginFranchisee1Res });
+  });
+
+  await page.route("**/*/api/franchise/2", async (route) => {
+    expect(route.request().method()).toBe("GET");
+    await route.fulfill({ json: getFranchises_franchisee1_store1 });
+  });
+
+  await page.goto("/");
+
+  await page
+    .getByLabel("Global")
+    .getByRole("link", { name: "Franchise" })
+    .click();
+  await page.getByRole("link", { name: "login", exact: true }).click();
+  await page.getByPlaceholder("Email address").fill("franchisee1@gmail.com");
+  await page.getByPlaceholder("Email address").press("Tab");
+  await page.getByPlaceholder("Password").fill("password123");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByRole("heading")).toContainText("Franchise 1");
+  await expect(page.locator("tbody")).toContainText("Store 1");
+  await expect(page.locator("tbody")).toContainText("0 ₿");
+});
